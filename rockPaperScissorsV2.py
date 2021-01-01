35f06e9c7aec779109bef62bf6177065ccd5e89d
Squidjigg <squidjigg80@outlook.com>
2021-01-01 19:26:58 +1100

    added emojis as options; added comments to code

--- rockPaperScissorsV2.py
+++ rockPaperScissorsV2.py
@@ -1,58 +1,69 @@
 import random
+import emoji
+# must install emoji package - 'pip install emoji'
 
-#rock = 1
-#paper = 2
-#scissors = 3
-options = {1:'Rock',2:'Paper',3:'Scissors'}
+#options = {1:'Rock',2:'Paper',3:'Scissors'}
 
-#print(options[1])
+# define items as emojis
+rock = emoji.emojize(':gem_stone:')     # use until :rock: is available
+paper = emoji.emojize(':roll_of_paper:')
+scissors = emoji.emojize(':scissors:')
 
-userMove = 0
+# add options to dictionary list
+options = {1:rock, 2:paper, 3:scissors}
+
+playerMove = 0
 cpuInput = 0
 
-userScore = 0
+playerScore = 0
 cpuScore = 0
 
 gameNumber = 0
 
 print('Lets Play...\n')
 
-while not (userScore == 5 or cpuScore == 5):
-    # print('Lets Play...\n')
+# continue game until someone gets 5 wins
+while not (playerScore == 5 or cpuScore == 5):
+    # let player choice their option
+    playerMove = int(input(f'(1) Rock {rock}, (2) Paper {paper}, (3) Scissors {scissors}\nEnter your choice: '))
+    
+    # prompt if wrong option entered
+    if playerMove >= 4 or playerMove <= 0:
+        print('Wrong option entered, try again.\n')
+        continue
 
-    userMove = int(input('(1) Rock, (2) Paper, (3) Scissors\nEnter your choice: '))
-
-    #print(userInput)
+    # randominse CPU choice
     cpuMove = random.randint(1,3)
 
     gameNumber += 1
 
     print(f'\n--- GAME {gameNumber} ---')
-    print(f'You: {options[userMove]}')
+    print(f'You: {options[playerMove]}')
     print(f'CPU: {options[cpuMove]}')
 
-    if userMove == 1 and cpuMove == 3:
+    # determine winner from each game by their choice
+    if playerMove == 1 and cpuMove == 3:
         result = 'You WIN: Rock beats Scissors!\n'
-        userScore = userScore + 1
-    elif userMove == 1 and cpuMove == 2:
+        playerScore = playerScore + 1
+    elif playerMove == 1 and cpuMove == 2:
         result = 'You LOSE: Paper beats Rock.\n'
         cpuScore = cpuScore + 1
-    elif userMove == 2 and cpuMove == 1:
+    elif playerMove == 2 and cpuMove == 1:
         result = 'You WIN: Paper beats Rock!\n'
-        userScore = userScore + 1
-    elif userMove == 2 and cpuMove == 3:
+        playerScore = playerScore + 1
+    elif playerMove == 2 and cpuMove == 3:
         result = 'You LOSE: Scissors beats Paper.\n'
         cpuScore = cpuScore + 1
-    elif userMove == 3 and cpuMove == 1:
+    elif playerMove == 3 and cpuMove == 1:
         result = 'You LOSE: Rock beats Scissors.\n'
         cpuScore = cpuScore + 1
-    elif userMove == 3 and cpuMove == 2:
+    elif playerMove == 3 and cpuMove == 2:
         result = 'You WIN: Scissors beats Paper!\n'
-        userScore = userScore + 1
+        playerScore = playerScore + 1
     else:
         result = "DRAW: both have the same!\n"
 
     print(f'{result}')
 
-    print(f'Your score = {userScore}')
+    print(f'Your score = {playerScore}')
     print(f'CPU score = {cpuScore}\n')
